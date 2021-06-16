8c8e7e903e177230638ac4e6918f7dd5d5f42bd3
LUNDI19 <michaelmcloughlin9@gmail.com>
2021-06-16 18:49:04 +0200

    User story 2 add new book mark feature

--- app.rb
+++ app.rb
@@ -17,6 +17,17 @@ get '/bookmarks' do
  
 end
 
+get '/bookmarks/new' do
+  erb :"bookmarks/new"
+end
+
+post '/bookmarks' do
+  url = params['url']
+  connection = PG.connect(dbname: 'bookmark_manager_test')
+  connection.exec("INSERT INTO bookmarks (url) VALUES('#{url}')")
+  redirect '/bookmarks'
+end
+
 run! if app_file == $0
 end
 
